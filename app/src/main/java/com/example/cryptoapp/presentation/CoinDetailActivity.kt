28bd90bc875b698b4f0f51bfd67f64d2a4804df7
Artem <unknow83@yandex.ru>
2022-02-17 16:31:19 +0500

    CoinDetailFragment.kt has been added in CoinDetailActivity.kt

--- app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
@@ -5,12 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
 import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: CoinVewModel
 
     private val binding by lazy {
         ActivityCoinDetailBinding.inflate(layoutInflater)
@@ -38,25 +37,11 @@ class CoinDetailActivity : AppCompatActivity() {
 
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
 
-        if (fromSymbol == EMPTY_SYMBOL) {
-            finish()
-            return
-        }
-
-        viewModel = ViewModelProvider(this)[CoinVewModel::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this) {
-            with(binding) {
-                tvFromSymbol.text = it.fromSymbol.toString()
-                tvToSymbol.text = it.toSymbol.toString()
-
-                tvDetailPrice.text = it.price.toString()
-                tvMinPrice.text = it.lowDay.toString()
-                tvMaxPrice.text = it.highDay.toString()
-                tvLastDeal.text = it.lastMarket.toString()
-                tvDetailLastUpdateTime.text = it.lastUpdate
-
-                Picasso.get().load(it.imageUrl).into(ivLogoCoinDetail)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 }
